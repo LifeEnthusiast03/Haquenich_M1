7a669e88799313de4d99d3f41d667e5923715c4e
SOUGATA SAHA <sahasougata820@gmail.com>
2025-07-08 00:48:34 +0530

    update team data

--- HQM1/HQM1_Backend/constant/teamdata.js
+++ HQM1/HQM1_Backend/constant/teamdata.js
@@ -5,7 +5,7 @@ export const  teamdata = [
   department: "Electronics & Communication Engineering",
   phone: "7063694560",
   email: "mondalnur@gmail.com",
-  image: "/Nurhak.jpg",
+  image: "/Nurhak_Mondal.jpg",
   projectRole: "Project Lead",
   specialization: "Battery Design, PCB Development, IoT Sensors"
 },
@@ -15,129 +15,98 @@ export const  teamdata = [
   department: "Infromation Technology",
   phone: "8420077098",
   email: "ritvik.sharma@example.com",
-  image: "/sougata.jpg", // Place 'ritvik.jpg' inside /public
-  projectRole: "Mechanical Design Lead",
-  specialization: "CAD Modeling, Bike Chassis, Kinematics"
+  image: "/Sombit.jpg", // Place 'ritvik.jpg' inside /public
+  projectRole: "Cloud & ML Engineer",
+  specialization: "AWS,NLP,Deep Learing"
 },
 {
-  name: "Sneha Das",
-  college: "Asansol Engineering College",
-  department: "Information Technology",
-  phone: "+91-9876543244",
-  email: "sneha.das@example.com",
-  image: "/sneha.jpg", // Place 'sneha.jpg' inside /public
-  projectRole: "Mobile App Developer",
-  specialization: "Flutter, Firebase, User Interface Design"
+  name: "Subhadip Layek",
+  college: "Institute of Engineering and Management",
+  department: "Electrical and Electronics Engineering",
+  phone: "+91-8617695092",
+  email: "subhadiplayek47@gmail.com",
+  image: "/Subhadip.jpg", // Place 'sneha.jpg' inside /public
+  projectRole: "Fronted Engineer",
+  specialization: "Express,Ejs,Bootstrap"
 },
 {
-  name: "Kunal Sengupta",
-  college: "IEM Kolkata",
-  department: "Electronics and Communication",
-  phone: "+91-9876543255",
-  email: "kunal.sengupta@example.com",
-  image: "/kunal.jpg", // Place 'kunal.jpg' inside /public
-  projectRole: "IoT Integration Expert",
-  specialization: "Microcontrollers, BLE Modules, GSM/GPS"
+  name: "Abir Sural",
+  college: "Institute of Engineering and Management",
+  department: "Electronics and Communication Engineering",
+  phone: "+91-7585830546",
+  email: "suralabir40@gmail.com",
+  image: "/Abir.webp", // Place 'kunal.jpg' inside /public
+  projectRole: "ProjectCo-lead  ",
+  specialization: "Web Development, Electrical & Electronics Designing, Project Management, BMS Integration "
 },
 {
-  name: "Rahul Verma",
-  college: "IEM Kolkata",
-  department: "Electronics and Communication",
-  phone: "+91-9876543255",
-  email: "rahul.verma@example.com",
-  image: "/rahul.jpg",
-  projectRole: "Firmware Engineer",
-  specialization: "Microcontrollers, Embedded C, Bluetooth LE"
-},
-{
-  name: "Priya Sen",
-  college: "Heritage Institute of Technology",
-  department: "Computer Science and Engineering",
-  phone: "+91-9876543266",
-  email: "priya.sen@example.com",
-  image: "/priya.jpg",
-  projectRole: "Cloud Integration Specialist",
-  specialization: "AWS, Node.js, Realtime Data Sync"
-},
-{
-  name: "Abhinav Gupta",
-  college: "Asansol Engineering College",
+  name: "Sharukh Khan",
+  college: "Institute of Engineering and Management",
   department: "Mechanical Engineering",
-  phone: "+91-9876543277",
-  email: "abhinav.gupta@example.com",
-  image: "/abhinav.jpg",
-  projectRole: "Frame Design Engineer",
-  specialization: "SolidWorks, 3D Printing, Stress Analysis"
+  phone: "+91-747750144",
+  email: "41.o.sharukhkhan@gmail.com",
+  image: "/sharukh_khan.jpg",
+  projectRole: "Cad & 3D Designer",
+  specialization: "Python, Solidworks, Fusion 360, Engineering Drawing, Ansys Analysis"
 },
 {
-  name: "Nikita Roy",
-  college: "IIT Dhanbad",
-  department: "Electrical Engineering",
-  phone: "+91-9876543288",
-  email: "nikita.roy@example.com",
-  image: "/nikita.jpg",
-  projectRole: "Powertrain Analyst",
-  specialization: "Battery Optimization, Regenerative Braking"
+  name: "Bratyabandhu Bhattacharyya",
+  college: "Institute of Engineering & Management",
+  department: "Electronics and Communication Engineering",
+  phone: "+91-8240005041",
+  email: "bratyabandhu@gmail.com",
+  image: "/bb.JPG",
+  projectRole: "Electrical Sub-system Designer",
+  specialization: "EasyEDA, Proteus, Electrical Relays"
 },
 {
-  name: "Arjun Chakraborty",
-  college: "NIT Durgapur",
-  department: "Computer Science and Engineering",
-  phone: "+91-9876543299",
-  email: "arjun.chakraborty@example.com",
-  image: "/arjun.jpg",
-  projectRole: "Backend Developer",
-  specialization: "Django, PostgreSQL, API Architecture"
+  name: "Mrinmoy Basu",
+  college: "Institute of Engineering & Management ",
+  department: "Electronics and Communication Engineering",
+  phone: "+91-7003352399",
+  email: "rkms2022mrinmoybasudetce25@gmail.com",
+  image: "/Mrinmoy_Basu.jpg",
+  projectRole: "Mechanical Team",
+  specialization: "Technical proficiency, problem-solving abilities, and strong communication and interpersonal skills"
 },
 {
-  name: "Tanya Mukherjee",
-  college: "Asansol Engineering College",
-  department: "Information Technology",
-  phone: "+91-9876543300",
-  email: "tanya.mukherjee@example.com",
+  name: "Sneha Roy ",
+  college: "University of Engineering and Managementk,olkata ",
+  department: "Electronics and Communication Engineering",
+  phone: "+91-7890232421",
+  email: "roy173636@gmail.com",
+  image: "/Sneha_Roy.jpg",
+  projectRole: "IoT subpart , UI / UX Designer",
+  specialization: "IoT, embedded system, VLSI,IC design, signal processing, Microcontroller, python ,C/ C++"
+},
+{
+  name: "Debabrata Ghosh",
+  college: "Institute of Engineering and Management",
+  department: "Electrical and Electronics Engineering",
+  phone: "+91-7439502673",
+  email: "debabratag542@gmail.com",
+  image: "/DEBABRATA_GHOSH.jpg",
+  projectRole: "Electrical Hardware and Simulation Lead",
+  specialization: "Python, Java, React, Node.js, Express, REST APIs, React Native, MongoDB, PostgreSQL, TensorFlow, PyTorch, scikit-learn, Pandas, NumPy, LLMs, DSA, Full-stack Dev, Git, Docker, Jupyter, Google Colab, Arduino, SCADA, PLC, PVsyst, Electrical Safety, Substation Ops, VCBs, Transformers"
+},
+{
+  name: "Priyadarshi Gupta ",
+  college: "Institute of Engineering and Management ",
+  department: "Electronics and Communication Engineering",
+  phone: "+91-6290359268",
+  email: "priyadarshigupta2004@gmail.com",
   image: "/tanya.jpg",
-  projectRole: "UI Designer",
-  specialization: "Figma, Material UI, Accessibility Design"
+  projectRole: "Battery Management system ",
+  specialization:"Analog circuits , circuit analysis and simulation "
 },
 {
-  name: "Rishi Kumar",
-  college: "Techno India University",
-  department: "Computer Engineering",
-  phone: "+91-9876543311",
-  email: "rishi.kumar@example.com",
-  image: "/rishi.jpg",
-  projectRole: "AI/ML Engineer",
-  specialization: "YOLO, TensorFlow, Image Recognition"
-},
-{
-  name: "Pooja Dasgupta",
-  college: "Asansol Engineering College",
-  department: "Electronics and Communication",
-  phone: "+91-9876543322",
-  email: "pooja.dasgupta@example.com",
-  image: "/pooja.jpg",
-  projectRole: "Sensor Specialist",
-  specialization: "GPS, Gyroscope, Ultrasonic Sensors"
-},
-{
-  name: "Samarjit Pal",
+  name: "Sougata Saha",
   college: "Jadavpur University",
-  department: "Mechanical Engineering",
-  phone: "+91-9876543333",
-  email: "samarjit.pal@example.com",
-  image: "/samarjit.jpg",
-  projectRole: "Testing Engineer",
-  specialization: "Load Testing, Terrain Simulation, QA"
-},
-{
-  name: "Ishita Ghosh",
-  college: "NIT Rourkela",
-  department: "Software Engineering",
-  phone: "+91-9876543344",
-  email: "ishita.ghosh@example.com",
-  image: "/ishita.jpg",
-  projectRole: "DevOps Engineer",
-  specialization: "CI/CD Pipelines, Docker, GitHub Actions"
+  department: "Infomation Technology",
+  phone: "+91-6296824383",
+  email: "sahasougata820@gmail.com",
+  image: "/sougata.jpg",
+  projectRole: "Software Team Lead",
+  specialization: "Express, React ,MongoDb,RestApi,Java,c++"
 }
-
 ];
